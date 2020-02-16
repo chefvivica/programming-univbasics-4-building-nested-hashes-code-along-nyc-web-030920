607241b391fcd049d992517c102da57f37dbba91
Vivica Ma <vivicacongma@gmail.com>
2020-02-16 02:56:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,27 +1,6 @@
 def first_challenge
    epic_tragedy = {
      Montague: {},
-     Capulet:{}
+     Capulet
    }
 end
-#
-In `lib/first_challenge.rb`, you're going to fill out the content of the
-`epic_tragedy` hash. The first level of our nested hash are the keys for the
-family names, "Montague" and "Capulet". We've made them symbols above, because
-symbols make better hash keys than strings.
-
-- Set the `epic_tragedy` variable equal to a hash with two key/value pairs. The
-  keys should be symbols of the family names (`:montague` and `:capulet`) and the
-  values should be empty hashes.
-
-At this point, we have a hash that looks like this:
-
-```ruby
-epic_tragedy = {
-  :montague => {},
-  :capulet => {}
-}
-```
-
-Now it's time to fill out the second level of our hash — the collection of data
-that constitutes the value hashes of each family name key.
